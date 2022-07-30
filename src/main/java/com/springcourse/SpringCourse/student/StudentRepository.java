1be5efba8b7f9b167160f0be99264d4156460a90
mpalacz <mpalacz00@gmail.com>
2022-07-30 22:49:08 +0200

    Connecting with database

---
+++ src/main/java/com/springcourse/SpringCourse/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.springcourse.SpringCourse.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
--- src/main/java/com/springcourse/SpringCourse/student/StudentService.java
+++ src/main/java/com/springcourse/SpringCourse/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.springcourse.SpringCourse.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +11,27 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    // wyświetla Studenta w JSONie
-    @GetMapping
-    public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Mariam",
-                "mariam.jamal@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21)
-        );
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
+    }
+
+    // wyświetla Studenta w JSONie
+//    @GetMapping
+//    public List<Student> getStudents() {
+//        return List.of(new Student(
+//                1L,
+//                "Mariam",
+//                "mariam.jamal@gmail.com",
+//                LocalDate.of(2000, Month.JANUARY, 5),
+//                21)
+//        );
+//    }
 }
